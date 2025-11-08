e242e2f9bae3a25864ba5653bedc57c5449e2eda
huongduongrlypr-source <huongduongrlypr@gmail.com>
2025-11-09 06:11:42 +0700

    Update rsbuild.config.mjs

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -5,52 +5,57 @@ import fs from 'fs/promises';
 import path from 'path';
 
 export default defineConfig({
-    plugins: [
-        pluginReact(),
-        {
-            name: 'plugin-htaccess-spa',
-            setup(api) {
-                api.onAfterBuild(async () => {
-                    const distPath = path.resolve('dist');
-                    const htaccessPath = path.join(distPath, '.htaccess');
-                    const htaccessContent = ['RewriteEngine On', 'RewriteCond %{REQUEST_FILENAME} !-f', 'RewriteCond %{REQUEST_FILENAME} !-d', 'RewriteRule ^ index.html [L]'].join('\n');
-                    try {
-                        await fs.access(distPath);
-                        await fs.writeFile(htaccessPath, htaccessContent);
-                        api.logger.info('htaccess build xong');
-                    } catch {
-                        api.logger.error('htaccess build fail');
-                    }
-                });
-            }
-        }
-    ],
-    tools: {
-        postcss: {
-            postcssOptions: {
-                plugins: [tailwindcss]
-            }
-        }
-    },
-    resolve: {
-        alias: {
-            '@': './src'
-        }
-    },
-    html: {
-        title: '',
-        favicon: './src/assets/images/icon.webp'
-    },
-    source: {
-        tsconfigPath: './jsconfig.json'
-    },
-    output: {
-        dataUriLimit: {
-            image: Number.MAX_SAFE_INTEGER,
-            svg: Number.MAX_SAFE_INTEGER,
-            font: Number.MAX_SAFE_INTEGER,
-            media: Number.MAX_SAFE_INTEGER,
-            assets: Number.MAX_SAFE_INTEGER
-        }
+  plugins: [
+    pluginReact(),
+    {
+      name: 'plugin-htaccess-spa',
+      setup(api) {
+        api.onAfterBuild(async () => {
+          const distPath = path.resolve('dist');
+          const htaccessPath = path.join(distPath, '.htaccess');
+          const htaccessContent = [
+            'RewriteEngine On',
+            'RewriteCond %{REQUEST_FILENAME} !-f',
+            'RewriteCond %{REQUEST_FILENAME} !-d', 
+            'RewriteRule ^ index.html [L]'
+          ].join('\n');
+          
+          try {
+            await fs.access(distPath);
+            await fs.writeFile(htaccessPath, htaccessContent);
+            api.logger.info('htaccess created');
+          } catch {
+            api.logger.error('htaccess creation failed');
+          }
+        });
+      }
     }
+  ],
+  tools: {
+    postcss: {
+      postcssOptions: {
+        plugins: [tailwindcss]
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': './src'
+    }
+  },
+  html: {
+    favicon: './src/assets/images/icon.webp'
+  },
+  source: {
+    tsconfigPath: './jsconfig.json' 
+  },
+  output: {
+    dataUriLimit: {
+      image: Number.MAX_SAFE_INTEGER,
+      svg: Number.MAX_SAFE_INTEGER,
+      font: Number.MAX_SAFE_INTEGER,
+      media: Number.MAX_SAFE_INTEGER,
+      assets: Number.MAX_SAFE_INTEGER
+    }
+  }
 });
